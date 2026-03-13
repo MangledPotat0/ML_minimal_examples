15a8c0af6ac4f9796fc1d3ef9011f2636b4841d4
Dawith Lim <lim185@purdue.edu>
2026-03-13 19:24:43 -0400

    Module doc

--- sk_logreg.py
+++ sk_logreg.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+sk_logreg.py
+Simple script to show the use of sklearn's logistic regression model on the
+iris dataset.
+"""
 
 # 3rd party module imports
 import numpy as np
@@ -11,15 +16,18 @@ from core.spark_etl import data_etl, numpify
 from core.plots import confusion_matrix
 
 def main(spark):
+    # Fetch data
     train, test = data_etl(spark, YType.DENSE) \
                             .randomSplit([0.75, 0.25])
     labels = [row.__getattr__("class")
               for row in train.select("class").distinct().collect()]
     train, test = numpify(train, test)
 
+    # Set up and train the regression model
     classifier = LogisticRegression(max_iter=200)
     classifier = classifier.fit(train[0], train[1])
 
+    # Evaluate model
     pred = classifier.predict_proba(test[0])
     confusion_matrix(test[1], np.argmax(pred, axis=1), labels,
                      figname="logreg")
