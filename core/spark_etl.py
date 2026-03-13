942fe4018eaaf3e0368294b82e3c063f21505b41
Dawith Lim <lim185@purdue.edu>
2026-03-13 19:25:18 -0400

    Added option to choose between dense and sparse (one-hot) y

--- core/spark_etl.py
+++ core/spark_etl.py
@@ -8,14 +8,19 @@ ETL workflow written in pyspark
 import os
 
 # 3rd party module imports
+import numpy as np
 from numpy import ndarray
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import OneHotEncoder, StandardScaler, StringIndexer, \
         VectorAssembler
 from pyspark.pandas import DataFrame
 from pyspark.sql.types import FloatType, StringType, StructField, StructType
+from pyspark.sql import SparkSession
 
-def data_etl(spark) -> DataFrame:
+# Local module imports
+from core.enums import YType
+
+def data_etl(spark: SparkSession, ytype: YType) -> DataFrame:
     """
     Performs ETL on the iris dataset from archive.ics.uci.edu/dataset/53/iris.
     Since iris.data file does not contain header row, a custom defined schema
@@ -35,8 +40,10 @@ def data_etl(spark) -> DataFrame:
     """
 
     # Bypass the ETL stages and read parquet directly if exists
-    if os.path.isdir("/app/workdir/data/iris.parquet"):
-        return spark.read.parquet("/app/workdir/data/iris.parquet")
+    if os.path.isdir(f"/app/workdir/data/iris_{ytype.name}.parquet"):
+        return spark.read.parquet(
+                f"/app/workdir/data/iris_{ytype.name}.parquet"
+        )
 
     # Make custom schema because the data file does not have a header
     schema = StructType([
@@ -52,42 +59,80 @@ def data_etl(spark) -> DataFrame:
             .load("/app/workdir/data/iris.data")
 
     # Prepare data transformation stages
-    indexer = StringIndexer(
-            inputCols=["class"],
-            outputCols=["class_idx"]
-    )
-    encoder = OneHotEncoder(
-            inputCols=["class_idx"],
-            outputCols=["class_encoded"],
-            dropLast=False
-    )
-    assembler = VectorAssembler(
+    stages = []
+    match ytype:
+        case YType.ONEHOT:
+            print("OOOO")
+            #Indexer
+            stages.append(StringIndexer(
+                    inputCols=["class"],
+                    outputCols=["class_idx"]
+            ))
+            #Encoder
+            stages.append(OneHotEncoder(
+                    inputCols=["class_idx"],
+                    outputCols=["class_encoded"],
+                    dropLast=False
+            ))
+        case YType.DENSE:
+            #Indexer
+            stages.append(StringIndexer(
+                    inputCols=["class"],
+                    outputCols=["class_encoded"]
+            ))
+    
+    # VectorAssembler
+    stages.append(VectorAssembler(
             inputCols=["sepal_length",
                        "sepal_width",
                        "petal_length",
                        "petal_width"],
             outputCol="features_prescale"
-    )
-    scaler = StandardScaler(
+    ))
+    #Scaler
+    stages.append(StandardScaler(
             inputCol="features_prescale",
             outputCol="features"
-    )
+    ))
 
-    pipeline = Pipeline(stages=[indexer, encoder, assembler, scaler])
+    pipeline = Pipeline(stages=stages)
 
     # Fit data and drop no longer needed columns for memory efficiency
     model = pipeline.fit(data)
     # It's inelegant compared to looping over names but avoids writing data to
     # variable 5 times.
-    data = model.transform(data) \
-            .drop("sepal_length") \
-            .drop("sepal_width") \
-            .drop("petal_length") \
-            .drop("petal_width") \
-            .drop("features_prescale") \
-            .drop("class_idx")
+    try:
+        data = model.transform(data) \
+                .drop("sepal_length") \
+                .drop("sepal_width") \
+                .drop("petal_length") \
+                .drop("petal_width") \
+                .drop("features_prescale") \
+                .drop("class_idx")
+    except ValueError:
+        data = model.transform(data) \
+                .drop("sepal_length") \
+                .drop("sepal_width") \
+                .drop("petal_length") \
+                .drop("petal_width") \
+                .drop("features_prescale") \
     # Load transformed data as parquet so that the same re-processing dosen't
     # have to happen at every run
     data.write.mode("overwrite").parquet("/app/workdir/data/iris.parquet")
 
     return data
+
+def numpify(*args):
+    """
+    Convert the dataframe columns into list of arrays for compatibility
+    """
+    return [
+        [
+            np.array(arg.select(label).collect()).squeeze()
+            for label in ["features", "class_encoded"]
+        ]
+        for arg in args
+    ]
+
+
+# EOF
